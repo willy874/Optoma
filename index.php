a79fad0769b09ee1908b1f0cb43b9b53c7c42916
willy <willy8742891@gmail.com>
2019-11-28 23:03:54 +0800

    架構建立

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'meta.php';?>
+    <title>奧圖碼</title>   
+    <link rel='stylesheet' type='text/css' media='all' href='./css/main.css'/>
+    <link rel='stylesheet' type='text/css' media='all' href='./css/page/home.css'/>
+</head>
+<body>
+<?php include 'header.php';?>
+    <main>
+<?php include 'home.html';?>
+    </main>
+<?php include 'footer.php'; ?>
+<script type="text/javascript" src="./js/jquery-3.4.0.min.js"></script>
+<script type="text/javascript" src="./js/jquery-migrate-1.4.1.min.js"></script>
+<script type="text/javascript" src="./js/jquery.fancybox.js"></script>
+<script type="text/javascript" src="./js/jquery.fancybox-media.js"></script>
+<script type="text/javascript" src="./js/jquery.fancybox-buttons.js"></script>
+<script type="text/javascript" src="./js/jquery.fancybox-thumbs.js"></script>
+<script type="text/javascript" src="./js/fontawesome.js"></script>
+<script type="text/javascript" src="./js/jquery.animateNumber.min.js"></script>
+<script type="text/javascript" src="./js/slick.min.js"></script>
+<script type="text/javascript" src="./js/layout.js"></script>
+<script type="text/javascript" src="./js/page/home.js"></script>
+</body>
+</html>
